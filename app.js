e89c07c54c62333092265498854086de83ef7247
Joshua Duke <dukejoshuae@gmail.com>
2020-04-24 18:57:05 -0400

    set up files

---
+++ app.js
@@ -0,0 +1,14 @@
+
+let express = require('express');
+let app = express();
+
+app.set("view engine", "ejs");
+
+
+app.get("/", (req, res)=>{
+  res.render("index");
+});
+
+app.listen(3000, process.env.PORT, ()=>{
+  console.log("server has started");
+})
