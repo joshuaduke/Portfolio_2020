c6b48393e8cd48fda89fea579c05a0b18e92c6cd
Joshua Duke <dukejoshuae@gmail.com>
2020-05-04 12:52:17 -0400

    added stylesheet

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ let express = require('express');
 let app = express();
 
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
 
 
 app.get("/", (req, res)=>{
   res.render("index");
 });
+
+
 
 app.listen(3000, process.env.PORT, ()=>{
   console.log("server has started");
